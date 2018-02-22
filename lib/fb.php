ee42a7facdc07b361caffb2544dec31e8c694e90
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-02-22 11:52:16 +0700

    Fix Bug

--- lib/fb.php
+++ lib/fb.php
@@ -1,11 +1,14 @@
 <?php
-class Reaction {
+class Reaction
+{
+	
 	function __construct()
 	{
 		//
 	}
-
-	private function url_get_contents ($Url) {
+	////////////// [alternatif file_get_contents] ///////////////
+	private function url_get_contents ($Url)
+	{
 		if (!function_exists('curl_init')){
 			die('CURL is not installed!');
 		}
@@ -16,8 +19,10 @@ class Reaction {
 		curl_close($ch);
 		return $output;
 	}
+	/////////////////////////////////////////////////////////////
 	
-private function fetch_value($str, $find_start, $find_end) {
+	private function fetch_value($str, $find_start, $find_end)
+	{
 		$start = strpos($str, $find_start);
 		if ($start === false) {
 				return "";
@@ -27,7 +32,8 @@ private function fetch_value($str, $find_start, $find_end) {
 		return trim(substr($str, $start + $length, $end));
 	}
 	
-	private function curl($url = '', $var = '',$echo = '',$ref = '',$header = false) {
+	private function curl($url = '', $var = '',$echo = '',$ref = '',$header = false)
+	{
 		global $config, $sock;
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_NOBODY, $header);
@@ -48,7 +54,8 @@ private function fetch_value($str, $find_start, $find_end) {
 		return $result;
 	}
 	
-	private function gender_check($userid,$token) {
+	private function gender_check($userid,$token)
+	{
 		$gen_url 	= 'https://graph.facebook.com/'.$userid.'?access_token='.$token;
 		$gen_data	= file_get_contents($gen_url);
 		$gen_data 	= json_decode($gen_data, true);
@@ -56,38 +63,41 @@ private function fetch_value($str, $find_start, $find_end) {
 		return $gender;
 	}
 	
-	public function send_reaction($user, $pass, $token, $r_male, $r_female, $max_status) {
+	public function send_reaction($user, $pass, $token, $r_male, $r_female, $max_status)
+	{
 		$get_post	= 'https://graph.facebook.com/me/home?fields=id,from&limit='.$max_status.'&access_token='.$token;
 		$get_post 	= file_get_contents($get_post);
 		$get_post 	= json_decode($get_post, true);
 		
-		foreach($get_post['data'] as $data) {
+		foreach($get_post['data'] as $data)
+		{
 			$this->curl("https://mobile.facebook.com/login.php?refsrc=https%3A%2F%2Fm.facebook.com%2F&login_try_number=1", "lsd=AVpI36s1&version=1&ajax=0&width=0&pxr=0&gps=0&dimensions=0&m_ts=1483804348&li=qg5xWAUZXopBIK0ABg1Dtlzt&email=$user&pass=$pass&login=Masuk");
 			
 			$stat_id	= $data['id'];
 			$post_id	= explode("_",$stat_id);
 			$r_start	= 'https://mobile.facebook.com/reactions/picker/?ft_id='.$post_id[1];			
 			$html 		= $this->curl($r_start);
-			$html 		= str_replace('&','&',$html);
+			$html 		= str_replace('&amp;','&',$html);
 			
-			if($user_gen == 'female') {
+			if($user_gen == 'female')
+			{
 				$r_females 	= '/ufi/reaction/?ft_ent_identifier='.$post_id[1].'&reaction_type='.$r_female;
-				
 				$r_female_e	= $this->fetch_value($html,$r_females,'" style="display:block">');
 				$r_female_l	= 'https://mobile.facebook.com/ufi/reaction/?ft_ent_identifier='.$post_id[1].'&reaction_type='.$r_female. $r_female_e;
 				$this->curl($r_female_l);
-				echo "$green [Male Status From ID] > $post_id[1] $user_gen => REACTED\n";
+	echo "-=[Status ID $post_id[1] => $user_gen => reacted]=-\n";
 			}else{
 				$r_males 	= '/ufi/reaction/?ft_ent_identifier='.$post_id[1].'&reaction_type='.$r_male;
-$r_male_e	= $this->fetch_value($html,$r_males,'" style="display:block">');
-$r_male_l	= 'https://mobile.facebook.com/ufi/reaction/?ft_ent_identifier='.$post_id[1].'&reaction_type='.$r_male. $r_male_e;
-$this->curl($r_male_l);
-echo "$green [Female Status From ID] > $post_id[1] => $user_gen => REACTED\n";
-}
-$this->curl('https://mobile.facebook.com/logout.php');
-$waktu=array("4","5","10","8","9","7");
-sleep(array_rand($waktu));
-}		
-}
+				$r_male_e	= $this->fetch_value($html,$r_males,'" style="display:block">');
+				$r_male_l	= 'https://mobile.facebook.com/ufi/reaction/?ft_ent_identifier='.$post_id[1].'&reaction_type='.$r_male. $r_male_e;
+				$this->curl($r_male_l);
+	echo "-=[Status ID $post_id[1] => $user_gen => reacted]=-\n";
+			}
+			
+		$this->curl('https://mobile.facebook.com/logout.php');
+			$waktu=array('4','5','6','7','8');
+			sleep(array_rand($waktu));
+		}		
+	}	
 }
 ?>
